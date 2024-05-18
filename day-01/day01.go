49d2034d38b347f9f5543a8c44f5f2b07c5fa834
Sebastian Rychert <sebastian_rychert@wp.pl>
2024-05-18 19:57:15 +0200

    day01 part two

--- day-01/day01.go
+++ day-01/day01.go
@@ -3,32 +3,58 @@ package day01
 import (
 	"bufio"
 	"strconv"
+	"strings"
 	"unicode"
 
 	utils "github.com/srychert/advent-of-code-2023/internal/utils"
 )
+
+func assignRune(runes *[]rune, char rune) {
+	switch len(*runes) {
+	case 0, 1:
+		*runes = append(*runes, char)
+	case 2:
+		(*runes)[1] = char
+	}
+}
 
 func Day01() int {
 	file := utils.GetInputFile("day-01")
 	defer file.Close()
 
 	sum := 0
+	var m = map[string]rune{
+		"one":   '1',
+		"two":   '2',
+		"three": '3',
+		"four":  '4',
+		"five":  '5',
+		"six":   '6',
+		"seven": '7',
+		"eight": '8',
+		"nine":  '9',
+	}
 
 	sc := bufio.NewScanner(file)
 	for sc.Scan() {
-		line := sc.Text()        // GET the line string
-		runes := make([]rune, 0) // initialize empty rune slice
+		line := sc.Text()
+		bound := 0
+		runes := make([]rune, 0)
 
-		for _, char := range line {
-			if !unicode.IsDigit(char) {
+		for i, char := range line {
+			if unicode.IsDigit(char) {
+				bound = i + 1
+				assignRune(&runes, char)
 				continue
 			}
 
-			switch len(runes) {
-			case 0, 1:
-				runes = append(runes, char)
-			case 2:
-				runes[1] = char
+			for k, v := range m {
+				if strings.Contains(line[bound:i+1], k) {
+					// move bound to last rune of matched substr
+					// e.g. twone -> one
+					bound = i
+					assignRune(&runes, v)
+				}
 			}
 		}
 
@@ -36,7 +62,7 @@ func Day01() int {
 			runes = append(runes, runes[0])
 		}
 
-		i, err := strconv.Atoi(string(runes)) // convert slice to integer
+		i, err := strconv.Atoi(string(runes))
 		utils.Check(err)
 		sum += i
 	}
